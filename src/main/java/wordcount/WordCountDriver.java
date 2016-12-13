2dc2f39e17c09a6825f7bcd7e218b16651eb6c36
unknown <1403035@nchc.org.tw>
2016-12-13 13:22:18 +0800

    initial commit

---
+++ src/main/java/wordcount/WordCountDriver.java
@@ -0,0 +1,41 @@
+package wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+/**
+ * Created by ogre0403 on 2016/2/26.
+ */
+public class WordCountDriver {
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+        String input = "data/wc.in";
+        String output = "output";
+
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setNumReduceTasks(1);
+
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+    }
+}
